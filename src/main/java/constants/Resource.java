eb6a4c81a3813abdf0aa045b20e6808ac7caf1e8
joaoalber <jooalberto1@gmail.com>
2019-06-05 22:48:41 -0300

    Handling bat files.

--- src/main/java/constants/Resource.java
+++ src/main/java/constants/Resource.java
@@ -5,10 +5,10 @@ import main.java.interfaces.concrete.CParameters;
 import main.java.interfaces.concrete.CSuppliers;
 
 public enum Resource {
-   WINDOWS(1), INI(2), HTTPD_CONF_PATH(3), HTTPD_CONF_FILE(4), INSTALL_SETTINGS(5), X64_CHECK(6);
+   WINDOWS(1), INI(2), HTTPD_CONF_PATH(3), HTTPD_CONF_FILE(4), INSTALL_SETTINGS(5), X64_CHECK(6), APACHE_PSQL_BAT(7), PHP_BAT(8);
 
    private int id;
-   private static String userPathChoice;
+   public static String USER_PATH_CHOICE;
 
    Resource(int id){
       this.id = id;
@@ -21,6 +21,8 @@ public enum Resource {
       else if(id == 4) return "httpd.conf";
       else if(id == 5) return "[InstallSettings]";
       else if(id == 6) return "C:\\Windows\\SysWOW64";
+      else if(id == 7) return "\\start-server\\startApachePSQL.bat";
+      else if(id == 8) return "\\start-server\\startPHP.bat";
       else throw new NullPointerException(("Id "+id+" don't exists"));
    }
 
@@ -28,6 +30,8 @@ public enum Resource {
       if(id == 1) return Constants.WINDOWS_PARAM;
       if(id == 2) return Constants.INI_PARAM;
       if(id == 6) return Constants.X64_CHECK_PARAM;
+      if(id == 7) return Constants.APACHE_PSQL_BAT_PARAM;
+      if(id == 8) return Constants.PHP_BAT_PARAM;
       else throw new NullPointerException(("Parameter with 'id = "+id+"' don't exists"));
    }
 
@@ -35,15 +39,15 @@ public enum Resource {
       private static final AParameters WINDOWS_PARAM = CParameters.getInstanceOfParameter(Resource.WINDOWS.getValue(), null);
       private static final AParameters INI_PARAM = CParameters.getInstanceOfParameter(
             Resource.WINDOWS.getValue()+ Resource.INI.getValue(), null);
+      private static final AParameters APACHE_PSQL_BAT_PARAM = CParameters.getInstanceOfParameter(
+            Resource.APACHE_PSQL_BAT.getValue()+ Resource.INI.getValue(), null);
+      private static final AParameters PHP_BAT_PARAM = CParameters.getInstanceOfParameter(
+            Resource.PHP_BAT.getValue()+ Resource.INI.getValue(), null);
       private static final AParameters X64_CHECK_PARAM = CParameters.getInstanceOfParameter(
             Resource.X64_CHECK.getValue(), null);
    }
 
-   public static String getUserPathChoice() {
-      return userPathChoice;
-   }
-
    public static void setUserPathChoice() {
-      Resource.userPathChoice = CSuppliers.getReadUserPathChoice().get();
+      Resource.USER_PATH_CHOICE = CSuppliers.getReadUserPathChoice().get();
    }
 }
